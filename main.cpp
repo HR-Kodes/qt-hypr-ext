aa833b3c0eea891f8fcdd10d2b460f4ebd4f6af8
Aylur <k.demeter@protonmail.com>
2024-07-19 02:12:21 +0530

    Updates: Battery Service Agent (optimized)

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 
 #include <LayerShellQt/Shell>
 #include <LayerShellQt/Window>
+#include <QWidget>
 #include <QApplication>
 #include <QByteArray>
 #include <QDebug>
@@ -28,27 +29,102 @@
 #include <QDBusReply>
 #include <QVariantMap>
 
-#include "main.moc" 
+
+class BatteryLevelWidget : public QWidget {
+    Q_OBJECT
+public:
+    BatteryLevelWidget(QProgressBar *progressBar, QWidget *parent = nullptr) : QWidget(parent), batteryPath("") {
+        label = new QLabel("Battery Level: N/A", this);
+        batteryIndicator = progressBar;
 
 
+        findBatteryPath();
+        if (!batteryPath.isEmpty()) {
+            connectToDbus();
+            updateBatteryLevel(); // Initial update
+        } else {
+            label->setText("Battery Level: N/A (Battery not found)");
+        }
+    }
 
-// Function to read battery level from Linux sysfs
-int readBatteryLevel() {
-    QFile file("/sys/class/power_supply/BAT0/capacity"); // Adjust BAT0 based on your system
-    if (!file.open(QIODevice::ReadOnly | QIODevice::Text))
-        return -1;
+private slots:
+    void updateBatteryLevel() {
+        if (batteryPath.isEmpty()) {
+            label->setText("Battery Level: N/A (Battery not found)");
+            return;
+        }
 
-    QTextStream in(&file);
-    QString line = in.readLine();
-    file.close();
+        QDBusInterface upower("org.freedesktop.UPower",
+                              batteryPath,
+                              "org.freedesktop.UPower.Device",
+                              QDBusConnection::systemBus());
 
-    bool ok;
-    int batteryLevel = line.toInt(&ok);
-    if (!ok)
-        return -1;
+        if (upower.isValid()) {
+            QVariant percentageVariant = upower.property("Percentage");
+            if (percentageVariant.isValid()) {
+                double batteryPercentage = percentageVariant.toDouble();
+                label->setText(QString("Battery Level: %1%").arg(batteryPercentage));
+                batteryIndicator->setValue(static_cast<int>(batteryPercentage));
+            } else {
+                qDebug() << "Failed to get battery percentage";
+                label->setText("Battery Level: N/A (Error getting percentage)");
+            }
+        } else {
+            qDebug() << "UPower interface is not valid";
+            label->setText("Battery Level: N/A (Invalid UPower interface)");
+        }
+    }
 
-    return batteryLevel;
-}
+private:
+    QLabel *label;
+    QProgressBar *batteryIndicator;
+    QString batteryPath;
+
+    void findBatteryPath() {
+        QDBusInterface upower("org.freedesktop.UPower",
+                              "/org/freedesktop/UPower",
+                              "org.freedesktop.UPower",
+                              QDBusConnection::systemBus());
+
+        if (upower.isValid()) {
+            QDBusReply<QList<QDBusObjectPath>> reply = upower.call("EnumerateDevices");
+            if (reply.isValid()) {
+                const auto &devices = reply.value();
+                for (const auto &device : devices) {
+                    QDBusInterface deviceInterface("org.freedesktop.UPower",
+                                                   device.path(),
+                                                   "org.freedesktop.UPower.Device",
+                                                   QDBusConnection::systemBus());
+                    if (deviceInterface.isValid()) {
+                        QVariant typeVariant = deviceInterface.property("Type");
+                        if (typeVariant.isValid() && typeVariant.toUInt() == 2) { // Type 2 indicates Battery
+                            batteryPath = device.path();
+                            qDebug() << "Found battery at path:" << batteryPath;
+                            break;
+                        }
+                    }
+                }
+            } else {
+                qDebug() << "Failed to enumerate devices:" << reply.error().message();
+            }
+        } else {
+            qDebug() << "UPower interface is not valid";
+        }
+    }
+
+    void connectToDbus() {
+        if (batteryPath.isEmpty()) return;
+
+        QDBusConnection::systemBus().connect(
+            "org.freedesktop.UPower",
+            batteryPath,
+            "org.freedesktop.DBus.Properties",
+            "PropertiesChanged",
+            this,
+            SLOT(updateBatteryLevel())
+        );
+    }
+};
 
 
 int main(int argc, char *argv[])
@@ -90,24 +166,27 @@ int main(int argc, char *argv[])
     });
 
 
-
     QProgressBar *batteryIndicator = new QProgressBar();
     batteryIndicator->setFixedSize(100, 24);
     batteryIndicator->setMinimum(0);
     batteryIndicator->setMaximum(100);
     batteryIndicator->setValue(0);
-    // batteryIndicator->setValue(readBatteryLevel());
     batteryIndicator->setObjectName("battery-indicator");
     batteryIndicator->setStyleSheet(styleSheet);
+
+
     mainLayout->addWidget(batteryIndicator);
+
 
     w.setLayout(mainLayout);
     w.setStyleSheet("QWidget { background-color: \"#171717\"; }");
     w.resize(barSize);
     qDebug() << w.size();
 
+
     QWindow *lwin = w.windowHandle();
     qDebug() << lwin;
+
 
     if (LayerShellQt::Window *lsh = LayerShellQt::Window::get(lwin)) {
         qDebug() << lsh;
@@ -118,17 +197,20 @@ int main(int argc, char *argv[])
         lsh->setAnchors(LayerShellQt::Window::AnchorBottom);
         lsh->setKeyboardInteractivity(LayerShellQt::Window::KeyboardInteractivityNone);
     }
-    QTimer batteryTimer;
-    batteryTimer.setInterval(1000); // Update interval: 1000 ms = 1 second
-    QObject::connect(&batteryTimer, &QTimer::timeout, [&]() {
-        int batteryLevel = readBatteryLevel();
-        batteryIndicator->setValue(batteryLevel);
-    });
-    batteryTimer.start();
+
+
+    BatteryLevelWidget widget(batteryIndicator);
 
 
     w.show();
-    
+
+
     // QTimer::singleShot(5000, &a, &QGuiApplication::quit);
     return a.exec();
 }
+
+
+
+
+
+#include "main.moc"
